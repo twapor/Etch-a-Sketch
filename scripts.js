dcf69273400874d7708388a858f90d620c84207e
twapor <totwagnerapor@gmail.com>
2024-02-28 21:26:56 +0200

    Updated user interface

--- scripts.js
+++ scripts.js
@@ -4,3 +4,56 @@ value.textContent = input.value;
 input.addEventListener('input', (event) => {
     value.textContent = event.target.value;
 });
+const eraser = document.querySelector('#erase');
+eraser.addEventListener('click', () => {
+    eraserOn = true;
+});
+const whitePaint = document.querySelector('#white');
+whitePaint.addEventListener('click', () => {
+    eraserOn = false;
+});
+let eraserOn = false;
+const resize = document.querySelector('#resize');
+resize.addEventListener('click', () => {resizeCanvas(input.value)})
+const canvasContainer = document.querySelector('#canvas-container');
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    const allPixels = document.querySelectorAll('.pixel');
+    allPixels.forEach(pixel => {
+        pixel.classList.remove('white');
+    });
+});
+
+function resizeCanvas(size){
+    const grid = document.querySelector('.canvas-grid');
+    canvasContainer.removeChild(grid);
+    createCanvas(size);
+}
+
+function createCanvas(size){
+    const grid = document.createElement('div');
+    grid.classList.add('canvas-grid');
+    canvasContainer.appendChild(grid);
+    for(let i= 0; i < size; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+        for(let j = 0; j < size; j++){
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mouseover', () => {
+                if(eraserOn){
+                    pixel.classList.remove('white');
+                }
+                else {
+                    pixel.classList.add('white');
+                }
+            })
+            row.appendChild(pixel);
+        }
+
+    }
+}
+
+createCanvas(16);
